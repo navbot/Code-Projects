c2606d9e85444f0f19dc4793ffd0663906892ebe
quagzy <divijsingh@gmail.com>
2017-04-15 18:45:02 +0530

    Add files via upload

--- Ashoka Autologin.py
+++ Ashoka Autologin.py
@@ -1,22 +1,42 @@
 # v0.1 of Ashoka Autologin
 #This project is covered under GNU GPLv3 licence
 import urllib
-login_page=""
-username=""
-password=""
-time_delay=0
-test_page="https://www.google.com"
+import urllib2
+import os.path
+import mechanize
 
-if username and password and login_page == "":
-    print "What is the login link?"
-    login_page=raw_input()
+login_page="http://10.1.0.100:8090/"
+password=""
+time_delay=""
+test_page="https://www.google.com"
+r=""
+
+br=mechanize.Browser()
+
+if os.path.isfile("Autologin_Data.txt")!=True:
     print "What is the username?"
     username=raw_input()
     print "What is the password?"
     password=raw_input()
     print "After how many hours do you want to log in again?"
-    time_delay=input()
+    time_delay=raw_input()
 
-    file_object= open("Autologin_Data.txt","w")
 
-with urllib.request.urlopen(login_page)
+    save_file= open("Autologin_Data.txt","w")
+    save_file.write(username+'\n')
+    save_file.write(password+'\n')
+    save_file.write(time_delay)
+    save_file.close()
+
+save_file=open("Autologin_Data.txt","r")
+username=save_file.readline()
+password=save_file.readline()
+time_delay=save_file.readline()
+save_file.close()
+
+login_data=urllib.urlencode({'username':username,
+                             'password':password})
+
+br.open(login_page,login_data)
+
+br.click(btnSubmit)
